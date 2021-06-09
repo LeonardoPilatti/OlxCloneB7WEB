06ac67297cc794dba9d8bd24d3dd9c670ac43b9c
Leonardo Pilatti <leonardopilatti14@gmail.com>
2021-06-08 21:57:14 -0300

    criando o header e alguns estilos para o cabeçalho

--- src/components/partials/Header/Header.js
+++ src/components/partials/Header/Header.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 
 const Header = () => {
-  return <header>Aqui é o header</header>;
+  return (
+    <header>
+      <section className="container">
+        <div className={styles.logo}>
+          <Link to="/">
+            <span className={styles.logo1}>O</span>
+            <span className={styles.logo2}>L</span>
+            <span className={styles.logo3}>X</span>
+          </Link>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="">Login</Link>
+            </li>
+            <li>
+              <Link to="">Cadastrar</Link>
+            </li>
+            <li>
+              <Link to="" className={styles.button}>
+                Poste um anúncio
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </header>
+  );
 };
 
 export default Header;
